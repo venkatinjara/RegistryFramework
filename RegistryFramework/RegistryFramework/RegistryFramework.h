91aab39380554bccc43a3a0771e095c02adc9169
Venkateswar Rao I <venkateswarrao.i@photon.in>
2022-04-07 18:59:55 +0530

    Initial commit

---
+++ RegistryFramework/RegistryFramework/RegistryFramework.h
@@ -0,0 +1,18 @@
+//
+//  RegistryFramework.h
+//  RegistryFramework
+//
+//  Created by Venkateswar Rao I on 07/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RegistryFramework.
+FOUNDATION_EXPORT double RegistryFrameworkVersionNumber;
+
+//! Project version string for RegistryFramework.
+FOUNDATION_EXPORT const unsigned char RegistryFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegistryFramework/PublicHeader.h>
+
+
---
+++ RegistryFramework/Service.swift
@@ -0,0 +1,15 @@
+//
+//  Service.swift
+//  RegistryFramework
+//
+//  Created by Venkateswar Rao I on 07/04/22.
+//
+
+import Foundation
+
+public class Service{
+    private init(){}
+    public static func doSomnthing() -> String{
+        return "Did call registry application"
+    }
+}
